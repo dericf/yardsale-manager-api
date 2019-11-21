cfd55f36cffff25db86f3b91b319c3fd38190780
Deric <dericgf@gmail.com>
2019-11-20 22:38:21 -0700

    Add some preliminary GQL queries

--- application/gql/queries.py
+++ application/gql/queries.py
@@ -1,5 +1,29 @@
 # Get User (by email)
-
-# Get User (by id)
-
+GET_USER_BY_EMAIL = '''query GetUserByEmail($email: String!) {
+  user(where: {email: {_eq: $email}}) {
+    uuid
+    id
+    created_at
+    updated_at
+    email
+    password_hash
+    role
+    confirmation_key
+    has_confirmed
+  }
+}'''
+# Get User (by uuid)
+GET_USER_BY_UUID = '''query GetUserByUUID($uuid: String!) {
+  user(where: {uuid: {_eq: $uuid}}) {
+    uuid
+    id
+    created_at
+    updated_at
+    email
+    password_hash
+    role
+    confirmation_key
+    has_confirmed
+  }
+}'''
 # Get Session (by user id)
