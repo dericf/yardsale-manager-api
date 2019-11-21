34df934e6b12370d65ceecff805ecd260366895a
Deric <dericgf@gmail.com>
2019-11-20 22:38:32 -0700

    Add basic Create User GQL Mutation

--- application/gql/mutations.py
+++ application/gql/mutations.py
@@ -1,5 +1,21 @@
 # Create User
+CREATE_USER = '''mutation InsertUser($email: String!, $passwordHash: String!, $confirmationKey: String!) {
+    insert_user(objects: {email: $email, password_hash: $passwordHash, confirmation_key: $confirmationKey}) {
+        returning {
+            uuid
+            id
+            created_at
+            updated_at
+            email
+            password_hash
+            role
+            confirmation_key
+            has_confirmed
+        }
+    }
+}
 
+'''
 # Update User
 
 # Delete User
